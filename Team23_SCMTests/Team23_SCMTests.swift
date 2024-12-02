b19d014ed0eddc1867f4f74b067ada91db1ca61f
Firdosh Alam <firdoshalam263@gmail.com>
2024-12-02 11:56:16 +0530

    Initial Commit

---
+++ Team23_SCMTests/Team23_SCMTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team23_SCMTests.swift
+//  Team23_SCMTests
+//
+//  Created by Batch - 1 on 02/12/24.
+//
+
+import Testing
+@testable import Team23_SCM
+
+struct Team23_SCMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
